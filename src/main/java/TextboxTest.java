7ce81c614e69ebd549ed87ad35effd72cc2816f1
Ha Phuong Thuy <thuy.hp@workwithjoy.vn>
2024-01-27 00:04:36 +0700

    code

---
+++ src/main/java/TextboxTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TextboxTest {
+    public void submitSuccessfully() {
+
+        // go url
+        // click element
+        // click vao menu textbox
+        // input data
+        // click submit
+
+        System.setProperty("webdriver.chrome.driver","/Users/thuy/Documents/Automation/03Tools");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+
+    }
+}
