12a1a696f688fcba2e6e59567284b02d2d58cf23
Ha Phuong Thuy <thuy.hp@workwithjoy.vn>
2024-01-31 10:16:07 +0700

    code

--- src/main/java/TextboxTest.java
+++ src/main/java/TextboxTest.java
@@ -1,20 +1,58 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TextboxTest {
+import static org.testng.Assert.*;
+
+public class TextboxTest extends Common{
+
     public void submitSuccessfully() {
+        getURL();
 
-        // go url
-        // click element
-        // click vao menu textbox
+        driver.findElement(By.xpath("//span[text()='Text Box']")).click();
         // input data
+        driver.findElement(By.id("userName")).sendKeys("thuy");
+        driver.findElement(By.id("userEmail")).sendKeys("thuy@mailsac.com");
+        driver.findElement(By.id("currentAddress")).sendKeys("currentAddress");
+        driver.findElement(By.id("permanentAddress")).sendKeys("permanentAddress");
+
         // click submit
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.findElement(By.id("submit")).click();
+        //submit.click();
 
-        System.setProperty("webdriver.chrome.driver","/Users/thuy/Documents/Automation/03Tools/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
-        driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+        //Get text hiển thị , So sánh với text nhập vào => giống nhau => pass
+        //Get text: Name:fdsfa
+        String originFullName = driver.findElement(By.id("name")).getText();
+        String originEmail = driver.findElement(By.id("email")).getText();
+        //Cắt chuỗi từ phải đến dấu ':'
+        int index = originFullName.indexOf(":");
+        String actualFullName = originFullName.substring(index + 1);
+        int indexemail = originEmail.indexOf(":");
+        String actualEmail = originEmail.substring(indexemail + 1);
 
+        assertEquals(actualFullName, "thuy");
+        assertEquals(actualEmail, "thuy@mailsac.com");
+    }
+
+    public void validEmail() {
+        getURL();
+        driver.findElement(By.id("userName")).sendKeys("thuy");
+        driver.findElement(By.id("userEmail")).sendKeys("thuymailsac.com");
+        driver.findElement(By.id("currentAddress")).sendKeys("currentAddress");
+        driver.findElement(By.id("permanentAddress")).sendKeys("permanentAddress");
+
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.findElement(By.id("submit")).click();
+
+        WebElement colorTextInputBox = driver.findElement(By.id("userEmail"));
+        String actualColor = colorTextInputBox.getAttribute("class");
+        String errorColor = "mr-sm-2 field-error form-control";
+        System.out.println(actualColor);
+
+        // Assert - TestNG : Compare actual vs expect
+        assertEquals(actualColor, errorColor);
     }
 }
