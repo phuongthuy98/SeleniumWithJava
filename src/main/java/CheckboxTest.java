19fc1679464f9658d03f7a8626e643a33ef5e960
Ha Phuong Thuy <thuy.hp@workwithjoy.vn>
2024-01-31 10:21:22 +0700

    fix

--- src/main/java/CheckboxTest.java
+++ src/main/java/CheckboxTest.java
@@ -1,9 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
 
 import java.util.List;
 
